6b142583801c4223feee8470edcaa3b1353dd652
Arhela <mykaldesign@gmail.com>
2017-01-08 15:18:36 -0600

    working on test app

--- Mykals Stuff/First Test/First Test/ViewController.swift
+++ Mykals Stuff/First Test/First Test/ViewController.swift
@@ -8,12 +8,15 @@
 
 import UIKit
 
-class ViewController: UIViewController , UITextFieldDelegate {
+class ViewController: UIViewController , UITextFieldDelegate , UIImagePickerControllerDelegate , UINavigationControllerDelegate {
 
     //MARK: Properties
-    @IBOutlet weak var NameTextField: UITextField!
-    @IBOutlet weak var NameDatabaseLabel: UILabel!
+    @IBOutlet weak var NameTextField: UITextField! // text field
+    @IBOutlet weak var NameDatabaseLabel: UILabel! // The header
+    @IBOutlet weak var PhotoImageVeiw: UIImageView!
     
+    
+    //MARK: Default Code
     override func viewDidLoad()
     {
         super.viewDidLoad()
@@ -26,12 +29,69 @@ class ViewController: UIViewController , UITextFieldDelegate {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+//MARK: UITextFieldDelegate
 
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool
+    {
+        //Hide Keyboard
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField)
+    {
+        NameDatabaseLabel.text = textField.text
+    }
+    
+//MARK: UIImagePickerControllerDelegate
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController)
+    {
+        // Dismiss the picker if the user canceled.
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any])
+    {
+        // The info dictionary may contain multiple representations of the image. You want to use the original.
+        guard let selectedImage = info[UIImagePickerControllerOriginalImage] as? UIImage else
+        {
+            fatalError("Expected a dictionary containing an image, but was provided the following: \(info)")
+        }
+        
+        // Set photoImageView to display the selected image.
+        PhotoImageVeiw.image = selectedImage
+        
+        // Dismiss the picker.
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
 //MARK: Actions
+    
     @IBAction func FullNameDatabse(_ sender: UIButton)
     {
         NameDatabaseLabel.text = "New Name Database"
     }
+    @IBAction func selectImageFromPhotoLibrary(_ sender: UITapGestureRecognizer)
+    {
+        //Hide Keyboard
+        NameTextField.resignFirstResponder()
+        
+        // UIImagePickerController is a view controller that lets a user pick media from their photo library.
+        let imagePickerController = UIImagePickerController()
+        
+        // Only allow photos to be picked, not taken.
+        imagePickerController.sourceType = .photoLibrary
+        
+        // Make sure ViewController is notified when the user picks an image.
+        imagePickerController.delegate = self
+        
+        present(imagePickerController, animated: true, completion: nil)
+    }
+    
+    
 
     
 }
