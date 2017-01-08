b111a0e98954c55f3ee75130a1b93ebaea4db899
Arhela <mykaldesign@gmail.com>
2017-01-07 23:28:58 -0600

    Moo

--- Mykals Stuff/First Test/First Test/ViewController.swift
+++ Mykals Stuff/First Test/First Test/ViewController.swift
@@ -8,18 +8,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController , UITextFieldDelegate {
 
-    override func viewDidLoad() {
+    //MARK: Properties
+    @IBOutlet weak var NameTextField: UITextField!
+    @IBOutlet weak var NameDatabaseLabel: UILabel!
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
+        NameTextField.delegate = self
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+//MARK: Actions
+    @IBAction func FullNameDatabse(_ sender: UIButton)
+    {
+        NameDatabaseLabel.text = "New Name Database"
+    }
 
+    
 }
 
