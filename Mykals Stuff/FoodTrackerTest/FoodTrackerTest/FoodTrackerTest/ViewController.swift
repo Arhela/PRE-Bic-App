4ea67018b728510053082f01aebc37277c4298e3
Arhela <mykaldesign@gmail.com>
2017-01-08 23:03:29 -0600

    Module 1 progress

---
+++ Mykals Stuff/FoodTrackerTest/FoodTrackerTest/FoodTrackerTest/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  FoodTrackerTest
+//
+//  Created by michael miller on 1/8/17.
+//  Copyright © 2017 TIAS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController
+{
+
+    
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
