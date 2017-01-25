4cefb892793351f86a3afb9055b52af91c93266e
MichaelShade <shade2128@gmail.com>
2017-01-25 13:14:47 -0600

    Implemented Save behavior in the Apple tut
    (I still have no real idea what some of these methods do. Will look
    into it

--- Shades Stuff/Xcode Test/TestyMcTestFace/TestyMcTestFace/MealTableViewController.swift
+++ Shades Stuff/Xcode Test/TestyMcTestFace/TestyMcTestFace/MealTableViewController.swift
@@ -155,10 +155,23 @@ class MealTableViewController: UITableViewController {
         
         if let sourceViewController = sender.source as? MealViewController,  let meal = sourceViewController.meal
         {
+            
+            
+            if let selectedIndexPath = tableView.indexPathForSelectedRow
+            {
+                // Update existing meal 
+                meals[selectedIndexPath.row] = meal
+                tableView.reloadRows(at: [selectedIndexPath], with: .none)
+            }
+            else{
+            
+
+            
             let newIndexPath = IndexPath(row: meals.count, section: 0)
             
             meals.append(meal)
             tableView.insertRows(at: [newIndexPath], with: .automatic)
+            }
         }
         
 
