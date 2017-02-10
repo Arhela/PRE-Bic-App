075c960e0d2e173a920a2358ddc3d9092e06575b
MichaelShade <shade2128@gmail.com>
2017-02-09 23:03:10 -0600

    Still trying to figure out date stuff

--- Shades Stuff/Xcode Test/DatePlayground.playground/Contents.swift
+++ Shades Stuff/Xcode Test/DatePlayground.playground/Contents.swift
@@ -17,6 +17,28 @@ var convertedDate = dateFormaterObj.string(from: currentDate as Date)
 var dateString = "24-12-2015 23:59"
 dateFormaterObj.dateFormat = "dd-MM-yyy HH:mm"
 var newDateobj = dateFormaterObj.date(from: dateString)
+newDateobj
+
+dateString = "Thu, 08 Oct 2015 09:22:33 GMT"
+dateFormaterObj.dateFormat = "EEE, dd MM yyy HH:mm:ss zzz"
+newDateobj = dateFormaterObj.date(from: dateString)
+
+// Dates for compatison test 
+dateFormaterObj.dateFormat = "EEE MMM dd yyy HH:mm"
+dateString = "Fri Mar 10 2017 05:00"
+var firstDateObj = dateFormaterObj.date(from: dateString)
+
+dateFormaterObj.dateFormat = "MMM dd yyy HH:mm"
+var eventStartTimeString = "Mar 10 2017 8:00"
+var eventEndTimeString = "Mar 10 2017 9:00"
+
+var eventStartTime = dateFormaterObj.date(from: eventStartTimeString)
+eventStartTime
+
+
+
+
+
 
 
 
