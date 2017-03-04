b7bdc4025d7da68e34e9d3560b84570abd2587d9
MichaelShade <shade2128@gmail.com>
2017-03-03 18:24:36 -0600

    Added TableViewCell class and hooked it up to prototype cell
    Working off of Meal view idea from the Apple Tut

---
+++ Base For APP/PREBIC-Information/PREBIC-Information/SchEventTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SchEventTableViewCell.swift
+//  PREBIC-Information
+//
+//  Created by Michael Shade on 3/3/17.
+//  Copyright © 2017 Unfazed Creations. All rights reserved.
+//
+
+import UIKit
+
+class SchEventTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
